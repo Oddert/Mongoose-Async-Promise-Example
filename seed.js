437d993bef9f9beab34df8aa219e86e0472a8492
Robyn <oddert@gmail.com>
2018-12-07 15:53:10 +0000

    fixed misspelled 'description' tag, added disp to show page

--- seed.js
+++ seed.js
@@ -7,9 +7,9 @@ require('dotenv').config()
 mongoose.connect(process.env.DATABASE)
 
 const seedBooks = [
-  { name: 'Design for the Real World', decription: 'Human Ecology and Social Change', author: 'Victor Papanek' }
+  { name: 'Design for the Real World', description: 'Human Ecology and Social Change', author: 'Victor Papanek' }
 , { name: '1000 Years of non linear history', author: 'Manuel DeLanda' }
-, { name: 'Thinking Objects', decription: 'Contemporary Approaches to Product Design', author: 'Tim Parsons' }
+, { name: 'Thinking Objects', description: 'Contemporary Approaches to Product Design', author: 'Tim Parsons' }
 ]
 
 Book.remove({}, err => {
