5697feb2476bb3298d2512aa633a502a02a86281
Robyn <oddert@gmail.com>
2018-12-12 16:26:55 +0000

    added the last modified routes

--- routes/books.js
+++ routes/books.js
@@ -52,7 +52,6 @@ router.get('/test', async (req, res, next) => {
 router.get('/new', (req, res) => res.render('books/new'))
 
 // Normal:
-
 // router.post('/books', (req, res) => {
 //   Book.create(req.body, (err, book) => {
 //     if (err) {
@@ -64,9 +63,7 @@ router.get('/new', (req, res) => res.render('books/new'))
 //   })
 // })
 
-
 // Callback:
-
 router.post('/', (req, res, next) => {
   const book = new Book(req.body)
 
@@ -87,57 +84,106 @@ router.post('/', (req, res, next) => {
 // })
 
 
+// Normal:
+// router.get('/:id', (req, res) => {
+//   console.log('Ln 89. get /:id...')
+//   Book.findById(req.params.id, (err, book) => {
+//     if (err) {
+//       console.log(err)
+//       res.status(500).json({ err })
+//     } else {
+//       res.render('books/show', { book })
+//     }
+//   })
+// })
+
+// Callback:
 router.get('/:id', (req, res) => {
-  console.log('Ln 48. get /:id...')
-  Book.findById(req.params.id, (err, book) => {
-    if (err) {
-      console.log(err)
-      res.status(500).json({ err })
-    } else {
-      res.render('books/show', { book })
-    }
+  console.log('Ln 102. get /:id...')
+  Book.findById(req.params.id)
+  .then(book => res.render('books/show', { book }))
+  .catch(err => {
+    console.log(err)
+    res.status(422).json({ err })
   })
 })
 
 
+// Normal:
+// router.get('/:id/edit', (req, res) => {
+//   console.log('Ln 114. get /:id/edit...')
+//   Book.findById(req.params.id, (err, book) => {
+//     if (err) {
+//       console.log(err)
+//       res.status(500).json({ err })
+//     } else {
+//       res.render('books/edit', { book })
+//     }
+//   })
+// })
+
+// Callback: (this demonstrates pre defined responces)
 router.get('/:id/edit', (req, res) => {
-  console.log('Ln 61. get /:id/edit...')
-  Book.findById(req.params.id, (err, book) => {
-    if (err) {
-      console.log(err)
-      res.status(500).json({ err })
-    } else {
-      res.render('books/edit', { book })
-    }
-  })
+  console.log('Ln 114. get /:id/edit...')
+
+  let handleResponce = book => res.render('books/edit', { book })
+
+  let handleError = err => {
+    console.log('handle error function', { err })
+    res.status(422).json({ err })
+  }
+
+  Book.findById(req.params.id)
+  .then(handleResponce)
+  .catch(handleError)
 })
 
+
+// Normal:
+// router.put('/:id', (req, res) => {
+//   console.log('Ln 144. put /:id...')
+//   const update = { ...req.body, updated: Date.now() }
+//   console.log(update)
+//   Book.findByIdAndUpdate(req.params.id, update, (err, book) => {
+//     if (err) {
+//       console.log(err)
+//       res.status(500).json({ err })
+//     } else {
+//       res.render('books/show', { book })
+//     }
+//   })
+// })
+
+// Callback:
 router.put('/:id', (req, res) => {
-  console.log('Ln 72. put /:id...')
+  console.log('Ln 159. put /:id...')
   const update = { ...req.body, updated: Date.now() }
-  console.log(update)
-  Book.findByIdAndUpdate(req.params.id, update, (err, book) => {
-    if (err) {
-      console.log(err)
-      res.status(500).json({ err })
-    } else {
-      res.render('books/show', { book })
-    }
-  })
+  Book.findByIdAndUpdate(req.params.id, update)
+  .then(book => res.render('books/show', { book }))
+  .catch(err => res.status(422).json({ err }))
 })
 
 
+// Normal:
+// router.delete('/:id', (req, res) => {
+//   console.log('Ln 169. delete /:id')
+//   Book.findByIdAndRemove(req.params.id, err => {
+//     if (err) {
+//       console.log(err)
+//       res.stauts(500).json({ err })
+//     } else {
+//       res.redirect('/books')
+//     }
+//   })
+// })
 
+
+// Callback:
 router.delete('/:id', (req, res) => {
-  console.log('Ln 87. delete /:id')
-  Book.findByIdAndRemove(req.params.id, err => {
-    if (err) {
-      console.log(err)
-      res.stuauts(500).json({ err })
-    } else {
-      res.redirect('/books')
-    }
-  })
+  console.log('Ln 183. delete /:id')
+  Book.findByIdAndRemove(req.params.id)
+  .then(() => res.redirect('/books'))
+  .catch(err => res.status(500).json({ err }))
 })
 
 module.exports = router
